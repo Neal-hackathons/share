ec21365ce38730f6abf01f345c0486b19044242b
DiasNabil <donrogue@protonmail.com>
2024-04-04 08:50:04 +0200

    ui creator

---
+++ frontend/share/src/lib/category.js
@@ -0,0 +1,32 @@
+
+const instruments = [
+    'synth',
+    'drums',
+    'vocals', 
+    'hats',
+    '808',
+    'snares',
+    'percussion',
+    'keys',
+    'guitar',
+    'shakers',
+    'piano',
+]
+
+const genres = [
+    'hip hop / r&b',
+    'pop / edm', 
+    'bass music',
+    'house / techno',
+    'live sounds',
+    'electronic',
+    'global',
+    'cinematic / fx'
+]
+
+const category = [
+    'one shot', 
+    'loop'
+]
+
+export {instruments, genres, category}
